e0c173542716f2fd0f4cf09d69c70d21a314aa3e
Rihard Rivis <rihard.rivis@proton.me>
2025-12-29 22:38:51 +0200

    dnsjava setup

--- sml/build.gradle.kts
+++ sml/build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("dnsjava:dnsjava:3.6.3")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+
     testImplementation(kotlin("test"))
 }
 
--- sml/src/main/kotlin/Main.kt
+++ sml/src/main/kotlin/Main.kt
@@ -1,14 +1,24 @@
 package ee.rivis.sml
 
+import org.xbill.DNS.Lookup
+import org.xbill.DNS.SimpleResolver
+import org.xbill.DNS.Type
 import java.security.MessageDigest
 
 fun main() {
-    getSearchHashCname("urn:oasis:names:tc:ebcore:partyid-type:unregistered:c4")
+    val resolver = SimpleResolver("127.0.0.1").apply { port=5353 }
+
+    val lookup = Lookup("B-8bb529042b90c77892efd34f7395f504.sml.test", Type.CNAME).apply { setResolver(resolver) }
+    val result = lookup.run()
+
+    result?.forEach { println(it.rdataToString()) }
+
 }
 
-fun getSearchHashCname(identifier: String) {
+fun generateHashCname(identifier: String): String {
     val md = MessageDigest.getInstance("MD5")
     md.reset()
     val messageDigest = md.digest(identifier.toByteArray())
     val hex = messageDigest.joinToString("") { "%02x".format(it) }
+    return "B-$hex"
 }
