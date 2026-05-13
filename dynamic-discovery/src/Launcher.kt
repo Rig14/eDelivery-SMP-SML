6f9616822de00fd10a01d616a530c292f80d6126
Rihard Rivis <rihard.rivis@proton.me>
2026-05-13 18:28:39 +0300

    potential improvements to the performance

--- dynamic-discovery/src/Launcher.kt
+++ dynamic-discovery/src/Launcher.kt
@@ -3,10 +3,13 @@ import klite.Server
 import klite.annotations.annotated
 import sml.SMLServer
 import smp.SMPRoutes
+import java.util.concurrent.Executors
 import kotlin.concurrent.thread
 
 fun main() {
-  Server().apply {
+  Server(
+    workerPool = Executors.newVirtualThreadPerTaskExecutor()
+  ).apply {
     context("/") {
       annotated<SMPRoutes>()
     }
