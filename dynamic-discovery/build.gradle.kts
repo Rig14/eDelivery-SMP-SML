1afa7f2634111e86b481c7fe5c62bc91073fd000
Rihard Rivis <rihard.rivis@proton.me>
2026-05-21 11:21:24 +0300

    version bumps

--- dynamic-discovery/build.gradle.kts
+++ dynamic-discovery/build.gradle.kts
@@ -8,18 +8,18 @@ repositories {
 }
 
 dependencies {
-  fun klite(module: String) = "com.github.keksworks.klite:klite-$module:1.8.0"
+  fun klite(module: String) = "com.github.keksworks.klite:klite-$module:1.8.2"
   implementation(klite("server"))
   implementation(klite("slf4j"))
 
   implementation("dnsjava:dnsjava:3.6.4")
 
-  testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.4")
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.4")
+  testImplementation("org.junit.jupiter:junit-jupiter:6.1.0")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.1.0")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.1.0")
   testImplementation("ch.tutteli.atrium:atrium-fluent:1.3.0-alpha-2")
-  testImplementation("io.mockk:mockk:1.14.6")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+  testImplementation("io.mockk:mockk:1.14.9")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
 }
 
 kotlin {
